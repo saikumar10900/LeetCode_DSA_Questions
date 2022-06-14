6e23cf73fafd976538a3ab87545f3df26e00705e
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2022-06-14 10:23:50 +0530

    Add files via upload

---
+++ PeakElementInMountainArray.java
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/peak-index-in-a-mountain-array/
+
+class PeakElementInMountainArray{
+    public int peakIndexInMountainArray(int[] arr) {
+        return peakElement(arr);
+        
+    }
+    int peakElement(int[] arr){
+        int start = 0;
+        int end = arr.length - 1;
+        while(start < end){
+            int mid = start + (end - start) / 2;
+            if(arr[mid] > arr[mid + 1]){
+                end = mid;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
